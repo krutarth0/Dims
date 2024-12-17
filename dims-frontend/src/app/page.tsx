5a1bf1dabb05b5e6440dd7fc411179e65959c04e
krutarth <krutarthzero@hotmail.com>
2024-12-17 14:21:32 -0500

    init

---
+++ dims-frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <ol>
+          <li>Getting started with DIMS...</li>
+          <li>Find a deployment place, select a database and doom 💣</li>
+        </ol>
+      </main>
+    </div>
+  );
+}
